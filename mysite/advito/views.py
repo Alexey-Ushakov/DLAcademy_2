c876cb16c1dc76055aa03d150c6d8c96de61689d
AlexeyUshakov <alexeyushakov42@gmail.com>
2021-10-10 15:39:53 +0300

    Init advito 0.0

--- mysite/advito/views.py
+++ mysite/advito/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    return HttpResponse('Продам гараж')
+
+def index2(request):
+    return HttpResponse('Продам гараж 2')
