d9241e82f94af7ade7e25a98d62cce57edd4fd68
AlexeyUshakov <alexeyushakov42@gmail.com>
2021-10-15 18:49:38 +0300

    Homework 4 1.0 Из функций в Классы(View) Post_delete, Post_edit, Post_delete_success

--- mysite/advito/urls.py
+++ mysite/advito/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from .views import (
-    AnnouncementView, post_detail,post_create, post_edit, post_delete, category, choise_category, IndexView
+    AnnouncementView, PostDetailView, PostCreateView, EditView, PostDelete, CategoryView, Category_choiseView, IndexView
 )
 
 app_name ='advito'
@@ -9,11 +10,12 @@ app_name ='advito'
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
     path('announcement/', AnnouncementView.as_view(), name="announcement"),
-    path('announcement/<int:post_id>/', post_detail, name="post_detail"),
-    path('announcement/create/', post_create, name="post_create"),
-    path('announcement/<int:post_id>/edit', post_edit, name="post_edit"),
-    path('announcement/<int:post_id>/delete', post_delete, name="post_delete"),
-    path('category/', category, name="category"),
-    path('category/<int:category_id>/', choise_category, name="choise_category")
+    path('announcement/<int:post_id>/', PostDetailView.as_view(), name="post_detail"),
+    path('announcement/create/', PostCreateView.as_view(), name="post_create"),
+    path('announcement/<int:post_id>/edit', EditView.as_view(), name="post_edit"),
+    path('announcement/<int:post_id>/delete', PostDelete.as_view(), name="post_delete"),
+    path('announcement/<int:post_id>/delete-success', TemplateView.as_view(template_name='advito/delete_success.html'), name="post_delete_success"),
+    path('category/', CategoryView.as_view(), name="category"),
+    path('category/<int:category_id>/', Category_choiseView.as_view(), name="choise_category")
 ]
 
